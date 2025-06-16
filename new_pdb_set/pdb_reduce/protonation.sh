5472827fd933dabb2f3b84060f8c0e7a7c46dd2f
Darian Yang <dty7@pitt.edu>
2025-06-16 13:37:27 +0200

    reran using add missing atoms feature

--- new_pdb_set/pdb_reduce/protonation.sh
+++ new_pdb_set/pdb_reduce/protonation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir reduce_out
+
 # Loop over p1 to p5
 for i in {1..5}; do
     prefix="p${i}"
@@ -8,7 +10,7 @@ for i in {1..5}; do
     if [[ -n "$input_file" ]]; then
         output_file="${prefix}.pdb"
         echo "Processing $input_file -> $output_file"
-        pdb4amber --reduce -i "$input_file" -o "$output_file"
+        pdb4amber --reduce --add-missing-atoms -i "$input_file" -o "reduce_out/$output_file"
     else
         echo "No file found for prefix $prefix"
     fi
